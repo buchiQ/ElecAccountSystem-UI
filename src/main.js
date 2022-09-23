ef6de824329a813b70ea4cf31c21dee73002f73b
zkn <1603832238@qq.com>
2022-09-23 09:28:42 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import router from "@/router";
+import request from "@/utils/request";
+import VueParticles from 'vue-particles'
 
+
+Vue.use(VueParticles)
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.prototype.request = request
+
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
